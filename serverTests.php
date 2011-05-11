66709f77d4978d5b021597c74845153a57df2647
Joe Wegner <jwegner@printarts.com>
2011-05-11 09:44:24 -0500

    Added AJAXified tests for Server Side (PHP) unit tests

--- serverTests.php
+++ serverTests.php
@@ -1,34 +1,44 @@
 <?
-var $test = false;
+$test = false;
 
 if(isset($_POST['test'])) {
 	$test = intval($_POST['test']);
 }
 
-var $tests = Array();
+$tests = Array(
+	"Pass Test",//1
+	"Fail Test"//2
+);
 
-array_push($tests, new test("Pass Function", "pass");
-array_push($tests, new test("Fail Function", "fail");
+if(is_numeric($test)) {
+	$methods = get_class_methods('testSuite');
+	$testval = call_user_func('testSuite::'.$methods[$test]);
+	$msg = Array();
+	$msg['pass'] = ($testval === true) ? true : false;
+	$msg['err'] = $testval;
 
-if($test) {
-	return call_user_func($tests[$test]->tester);
+	echo json_encode($msg);
+	die();
 } else {
-	
+	echo json_encode($tests);
+	die();
 }
 
+class testSuite {
+	public $titles = Array();
 
-function pass() {
-	return true;
-}
-function fail() {
-	return "This function was supposed to fail";
-}
-class test {
-	public var $titl;
-	public var $tester;
-	function __construct($ttl, $funct) {
-		$this->titl = $ttl;
-		$this->tester = $funct;
+	//TEST 1
+	//Title: Pass Test
+	//Should just pass
+	static function passTest() {
+		return true; //just pass
+	}
+
+	//TEST 2
+	//Title: Fail Test
+	//Should just fail
+	static function failTest() {
+		return "This test was supposed to fail"; //just fail
 	}
 }
 ?>
